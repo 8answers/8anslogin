48456810408a2194b1d39c272e5dd53f4b00a7d3
8answers <ans8wer@gmail.com>
2024-12-09 15:13:59 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-fullHOM"><div className="2xl">
+     HOME PAGE </div></div>
+  );
+}
