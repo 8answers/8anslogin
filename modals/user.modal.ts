48e7ec1a5a6a185b22668a11b0ee8369f7e1a0bc
8answers <ans8wer@gmail.com>
2024-12-09 15:39:04 +0530

    Add files via upload

---
+++ modals/user.modal.ts
@@ -0,0 +1,36 @@
+import { Schema, model,models } from "mongoose";
+const Userschema=new Schema(
+    {
+      clerkId:{
+        type:String,
+        required:true,
+        unique:true,
+
+      } ,
+      email:{
+        type:String,
+        required:true,
+
+      } ,
+      username:{
+        type:String,
+        required:true,
+      },
+      photo:{
+        type:String,
+        required:true,
+      },
+      firstName:{
+        type:String,
+
+      },
+      lastName:{
+        type:String,
+      },
+
+
+    }
+);
+
+const User=models?.User || model("User",Userschema);
+export default User;
